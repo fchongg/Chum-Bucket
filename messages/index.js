ec7a83fddc72d6855d450f858eb084b282c6257b
fchongg <fchongg@gmail.com>
2017-03-26 07:42:04 -0700

    working bot

--- messages/index.js
+++ messages/index.js
@@ -7,6 +7,11 @@ http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 "use strict";
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
+var http = require('http');
+var https = require('https');
+var request = require("superagent");
+var url = require("url");
+var uuid = require("uuid");
 
 var useEmulator = (process.env.NODE_ENV == 'development');
 
@@ -18,6 +23,12 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
 });
 
 var bot = new builder.UniversalBot(connector);
+
+var AzureSearch = require('azure-search');
+var client = AzureSearch({
+    url: "https://frances-bot.search.windows.net",
+    key:"61B7D0187DBCC84B1F2B5C1473B8E6A4"
+});
 
 // Make sure you add code to validate these fields
 var luisAppId = process.env.LuisAppId;
@@ -36,19 +47,72 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 */
 
 .matches('None', (session, args) => {
-    session.send('Hi this is your intent handler, you said \'%s\'.', session.message.text);
+    // session.send('Hi this is your intent handler, you said \'%s\'.', session.message.text);
 })
     
-.matches('weather', (session, args) => {
-    session.send('You asked for weather' + JSON.stringify(args));
+// .matches('weather', (session, args) => {
+//     session.send('You asked for weather' + JSON.stringify(args));
+// })
+
+.matches('bye', (session, args) => {
+    session.send('Bye, have a nice day!');
 })
 
 .matches('greeting', (session, args) => {
     session.send('Hi!');
 })
+
+.matches('search', (session, args) => {
+    const valid_urls = args.entities.filter((entity)=>{
+        return entity.type=='search_key';
+    });
+    
+    const search_string = valid_urls.map((entity)=>entity.entity).join(" ");
+    client.search("htmldata", {search:search_string}, (err, results,raw)=>{
+            if (err){
+                session.send(JSON.stringify(err));
+            }
+            else {
+                var links = (results.map((res)=>res.link));
+                
+                var unique = links.filter(function(elem, index, self) {
+                    return index == self.indexOf(elem);
+                })
+                
+                session.send(JSON.stringify(unique));
+            }
+    });
+    
+    
+})
+
+.matches('url', (session, args) => {
+    
+    const valid_urls = args.entities.filter((entity)=>{
+        return entity.type=='urlValid';
+    });
+    
+    
+    valid_urls.forEach((validUrl)=>{
+        var urlString = validUrl.entity;
+        urlString =  urlString.split(" ").join("");
+        urlString = url.parse(urlString).href.replace("%3C", "").replace("%3E", "");
+
+        saveUrl(session, urlString, (err, results)=>{
+            if (err){
+                // session.send(JSON.stringify(err));
+            }
+            else {
+                // session.send(JSON.stringify(results));
+            }
+        })
+    })
+    
+   
+})
     
 .onDefault((session) => {
-    session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+    // session.send('Sorry, I did not understand \'%s\'.', session.message.text);
 });
 
 bot.dialog('/', intents);    
@@ -64,3 +128,62 @@ if (useEmulator) {
     module.exports = { default: connector.listen() }
 }
 
+function http_Get(url_1,  next){
+    try
+    {
+        request
+   .get(url_1)
+   .end(function(err, res){
+     if (err || !res.ok) {
+         next(err);
+     } else {
+         
+        const title = getTag(res.text, 'title');
+        const body = getTag(res.text, 'body');
+        next(null,{title, body});
+     }
+   });
+    }
+    catch (err){
+         next(err);
+    }
+} 
+
+
+function saveUrl(session, validUrl, next){
+    
+    http_Get( validUrl,  ( err, response)=>{
+        if (err){
+            return next(err);
+        }
+        const linktosave = {
+            "id": uuid.v4(), 
+            "conversationId": session.message.address.conversation.id,
+            "title": response.title ,
+            "body": response.body, 
+            "link":validUrl
+            
+        };
+        client.addDocuments("htmldata", [linktosave], next);
+        
+    })
+    
+}
+
+
+function getTag(res_text, tag){
+    var start = res_text.indexOf(`<${tag.toLowerCase()}`);
+    var end = res_text.indexOf(`</${tag.toLowerCase()}>`);
+
+    if (start == -1){
+        start = res_text.indexOf(`<${tag.toUpperCase()}`);
+        end = res_text.indexOf(`</${tag.toUpperCase()}>`);
+    }
+
+    if (start == -1){
+        return "";
+    }
+
+    return res_text.substring(start+(tag.length), end);
+}
+ 
